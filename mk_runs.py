9339163fc7ec6652da8947c05798accacd2944b7
Peter Teuben <teuben@gmail.com>
2023-05-09 22:35:15 -0400

    add may 8

--- mk_runs.py
+++ mk_runs.py
@@ -11,7 +11,8 @@ project="2023-S1-MX-28"
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
 
-on["TYC_2597_735_1"] =  [ 109471, 109472,]                       # may 4
+on["TYC_2597_735_1"] =  [ 109471, 109472,                        # may 4
+                          109969, 109970,]                       # may 8
 
 pars1 = {}
 
@@ -19,7 +20,7 @@ pars1["TYC_2597_735_1"] = ""
 
 pars2 = {}
 
-pars2["TYC_2597_735_1"] = "pix_list=-3"
+pars2["TYC_2597_735_1"] = "pix_list=-13"
 
 
 if __name__ == '__main__':    
